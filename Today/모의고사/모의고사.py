dcbc57ddaa05873ad8a2cba3cde15156b429cc98
yunjs24 <yunjs24@gmail.com>
2022-07-15 01:46:15 +0900

    Refactoring 모의고사

--- Today/모의고사/모의고사.py
+++ Today/모의고사/모의고사.py
@@ -5,14 +5,16 @@ pattern=['12345','21232425','3311224455']
 def solution(answers):
     answer = []
     n = len(answers)
-    examinees = [0,0,0]
-    sizes = [len(pattern[0]),len(pattern[1]),len(pattern[2])]
+    examinees = [0] * len(pattern)
+    sizes = []
+    for i in range(len(pattern)):
+        sizes.append(len(pattern[i]))
     for i in range(n):
-        for j in range(0,3):
+        for j in range(0,len(pattern)):
             if (answers[i] == int(pattern[j][i % sizes[j]])):
                 examinees[j] += 1
     twistedFate = max(examinees)
-    for i in range(3):
+    for i in range(len(pattern)):
         if (twistedFate == examinees[i]):
             answer.append(i + 1)
     return answer
