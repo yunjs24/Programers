1bb520ab276c97f066afc5b5bad70a1135fc418e
yunjs24 <yunjs24@gmail.com>
2022-09-22 23:50:06 +0900

    Add best_set.py

---
+++ Practice/best_set.py
@@ -0,0 +1,19 @@
+def solution(n, s):
+    answer = []
+
+    if s < n:
+        return [-1]
+
+    for _ in range(n):
+        answer.append(s // n)
+
+    indexs = len(answer) - 1
+
+    for i in range(s - sum(answer)):
+        answer[indexs] += 1
+        indexs -= 1
+
+    return answer
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/12938
